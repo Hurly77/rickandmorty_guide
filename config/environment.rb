d2d55f649a1778b31860afdb229610d8699837da
C.J Levey <cameron.leverett@yahoo.com>
2020-03-23 18:28:44 -0700

    second commit

---
+++ config/environment.rb
@@ -0,0 +1,7 @@
+require 'httparty'
+require 'pry'
+
+#require_relative file that live in app
+require_relative "../lib/rickandmorty_guide/api"
+require_relative "../lib/rickandmorty_guide/cli"
+require_relative "../lib/rickandmorty_guide/rickandmorty_guide.rb/version"
